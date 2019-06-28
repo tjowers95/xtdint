4851aacad7e4e98de9b9bebbc98ab83889e3c2a0
Tyler Jowers <Jowers.tc@protonmail.com>
2019-06-28 03:03:27 -0400

    added to shift

--- xtdint.c
+++ xtdint.c
@@ -1,19 +1,98 @@
 #include "xtdint.h"
-/* Input 8-bit array - v - along with a 16-bit "size-in-bytes" - s - and return a 2048-bit representation*/
-u2048* U2048(u8* v, u16 s)
+#include <stdio.h>
+#include <stdlib.h>
+u2048 *
+U2048(u8 *v, u16 s)
 {
     if (s > 256)
         return NULL;
-        
-    u2048* w = (u2048*)malloc(256); u8 d = 32 - (s/8);
-    
+
+    u2048 *w = (u2048 *)malloc(256);
+    u8 d = 32 - (s / 8);
+
     for (u8 i = 0; i < d; i++)
         w->n[i] = 0;
-        
-    for (u16 i = 1; i <= s; i+=8)
-        w->n[(i/8) + d] = ((u64)v[i - 1] << 56) | ((u64)v[i] << 48) | ((u64)v[i + 1] << 40) | ((u64)v[i + 2] << 32) | ((u64)v[i + 3] << 24) | ((u64)v[i + 4] << 16) | ((u64)v[i + 5] << 8) | (u64)v[i + 6];
-        
+
+    for (u16 i = 1; i <= s; i += 8)
+        w->n[(i / 8) + d] = ((u64)v[i - 1] << 56) | ((u64)v[i] << 48) | ((u64)v[i + 1] << 40) | ((u64)v[i + 2] << 32) | ((u64)v[i + 3] << 24) | ((u64)v[i + 4] << 16) | ((u64)v[i + 5] << 8) | (u64)v[i + 6];
+
     return w;
 }
 
-s2048* S2048(u8* v, u16 s){ return U2048(v, s);}
+void printx(u2048 *v)
+{
+    u8 *w = (u8 *)v->n;
+    for (int i = 1; i <= 256; i++)
+    {
+        if (w[i - 1] & 0xf0)
+            printf("%x ", w[i - 1]);
+        else
+            printf("0%x ", w[i - 1]);
+
+        if (!(i % 8))
+            printf("\n");
+    }
+    printf("\n");
+}
+
+void printb(u2048 *v)
+{
+    for (int i = 0; i < 32; i++)
+    {
+        for (int j = 63; j > -1; j--)
+        {
+            if (v->n[i] & ((u64)1 << j))
+                printf("1");
+            else
+                printf("0");
+            if (!(j % 8))
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void shift(u2048 *v, s32 n)
+{
+    s32 u = n / 64; // this will be the number of array positions that will be shifted
+    s32 n0 = n;
+    s32 chk_sign = n & 0x80000000;
+
+    if (!chk_sign) // positive
+    {
+        u64 ps0 = 0, ps1 = 0; // previous state
+        s32 u0 = u;           // save u state
+        for (u8 i = 0; i < u; i++)
+        {
+            for (u8 j = 0; j < 32; j++)
+            {
+                ps1 = v->n[j];
+                v->n[j] = ps0;
+                ps0 = ps1;
+            }
+        }
+
+        n0 -= (u * 64);
+    }
+    else // negative
+    {
+        u64 ps0 = 0, ps1 = 0;
+        s32 u0 = u;
+        for (u8 i = 0; i < -u; i++)
+        {
+            for (s8 j = 31; j > -1; j--)
+            {
+                ps1 = v->n[j];
+                v->n[j] = ps0;
+                ps0 = ps1;
+            }
+        }
+        while (++u0)
+            v->n[32 + u0] ^= v->n[32 + u0];
+    }
+}
+
+void endian_mod(void *v, int n)
+{
+}
+s2048 *S2048(u8 *v, u16 s) { return U2048(v, s); }
--- xtdint.h
+++ xtdint.h
@@ -1,12 +1,27 @@
 #ifndef _XTDINT_H_
 #define _XTDINT_H_
-typedef unsigned char u8; typedef char s8;typedef unsigned short u16; typedef short s16;
-typedef unsigned int u32; typedef int s32;typedef unsigned long long u64; typedef long long s64;
-typedef struct u2048 {u64 n[32];} u2048; typedef u2048 s2048;
-u2048* U2048(u8* v, u16 s); s2048* S2048(u8* v, u16 s);
-void shift(u2048* v, s16 n);
-void add(u2048* v, u2048* w); void add_s(s2048* v, s2048* w);
-void sub(u2048* v, u2048* w); void sub_s(s2048* v, s2048* w);
-void mul(u2048* v, u2048* w); void mul_s(s2048* v, s2048* w);
-#define to_u64(a,b,c,d,e,f,g,h) ((((u64)a) << 56) | (((u64)b) << 48) | (((u64)c) << 40) | (((u64)d) << 32) | (((u64)e) << 24) | (((u64)f) << 16) | (((u64)g) << 8) | (((u64)g) << 8) | ((u64)h))
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned short u16;
+typedef short s16;
+typedef unsigned int u32;
+typedef int s32;
+typedef unsigned long long u64;
+typedef long long s64;
+typedef struct u2048
+{
+    u64 n[32];
+} u2048;
+typedef u2048 s2048;
+extern u2048 *U2048(u8 *v, u16 s);
+extern s2048 *S2048(u8 *v, u16 s);
+extern void shift(u2048 *v, s32 n);
+extern void add(u2048 *v, u2048 *w);
+extern void add_s(s2048 *v, s2048 *w);
+extern void sub(u2048 *v, u2048 *w);
+extern void sub_s(s2048 *v, s2048 *w);
+extern void mul(u2048 *v, u2048 *w);
+extern void mul_s(s2048 *v, s2048 *w);
+extern void printb(u2048 *v);
+#define to_u64(a, b, c, d, e, f, g, h) ((((u64)a) << 56) | (((u64)b) << 48) | (((u64)c) << 40) | (((u64)d) << 32) | (((u64)e) << 24) | (((u64)f) << 16) | (((u64)g) << 8) | (((u64)g) << 8) | ((u64)h))
 #endif
